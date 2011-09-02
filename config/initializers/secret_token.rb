aed431ace5660ffde43cdef3d44c1530fd634547
Joe Woodhouse <joewoodhouse30@hotmail.com>
2011-09-02 04:20:36 +0100

    SweepBook App first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sweepbook::Application.config.secret_token = '763ed877948342d529c1b0a9a452a70db3555a9842d25726f80905b9598c07122f932acd8279f693e5f5cb11b4edf6c77b7206be01b0566886a4c074a9d483d7'
