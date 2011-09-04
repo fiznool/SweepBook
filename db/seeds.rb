4f6560bfab3659f1e54712dcbf028787d202f245
Joe Woodhouse <joewoodhouse30@hotmail.com>
2011-09-04 08:21:42 +0100

    Use generated event.id in creating competitors

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
 timeNow = Time.now
-Event.create(:id => 1, :name => 'GNR 2011', :date => timeNow, :info => 'Joe and Spens amazing GNR 2011', :forcewallpostsoff => false, :forceanonymousdonations => false, :time_min => 120, :time_max => 180, :time_interval => 1, :donation_link => 'http://www.justgiving.com/geeksgorunning')
-Competitor.create(:id => 1, :event_id => 1, :facebook_id => '123456', :name => 'Joe') 
-Competitor.create(:id => 2, :event_id => 1, :facebook_id => 'fiznool', :name => 'Spen')
+e = Event.create(:id => 1, :name => 'GNR 2011', :date => timeNow, :info => 'Joe and Spens amazing GNR 2011', :forcewallpostsoff => false, :forceanonymousdonations => false, :time_min => 120, :time_max => 180, :time_interval => 1, :donation_link => 'http://www.justgiving.com/geeksgorunning')
+Competitor.create( :event_id => e.id, :facebook_id => '123456', :name => 'Joe') 
+Competitor.create( :event_id => e.id, :facebook_id => 'fiznool', :name => 'Spen')
