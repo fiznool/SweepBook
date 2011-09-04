1a7c6538a0e170e2b3085a46fb2deeed2724de55
Joe Woodhouse <joewoodhouse30@hotmail.com>
2011-09-04 11:02:38 +0100

    Try to fix migration

--- db/migrate/20110904044221_add_name_to_competitor.rb
+++ db/migrate/20110904044221_add_name_to_competitor.rb
@@ -1,6 +1,6 @@
 class AddNameToCompetitor < ActiveRecord::Migration
   def self.up
-    add_column :competitors, :name, :string
+#    add_column :competitors, :name, :string
   end
 
   def self.down
