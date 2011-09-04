e4d7505f33d9f619cb81b89d245134f363739df3
Joe Woodhouse <joewoodhouse30@hotmail.com>
2011-09-04 04:05:16 +0100

    First go at publishing a donation

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -2,6 +2,8 @@ class DonationsController < ApplicationController
   def create 
 
     donations= Array.new
+    donation_ids = Array.new
+
     if( params.has_key?('json') )
       @json = ActiveSupport::JSON.decode(params[:json])
       @json.each do |c|
@@ -25,14 +27,27 @@ class DonationsController < ApplicationController
     end
 
     donations.each do |don|
-      unless don.save
+      if don.save
+        donation_ids.push(don.id)
+      else
         render :json => don.errors, :status => 400
+        return
       end
     end 
-    render :json => {}, :status => :ok
+    render :json => donation_ids , :status => :ok
   end
 
   def confirm
+  end
+
+  def publish
+    @donation = Donation.find(params[:donation_id])
+    if current_facebook_user
+      current_facebook_user.fetch
+      current_facebook_user.feed_create(\
+        Mogli::Post.new( :name => 'test', :link => 'test', :description => 'test' ) );      
+    end
+    render :json => { 'test' => current_facebook_user }, :status => :ok
   end
 
   def destroy
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @event_id = 1
     @event = Event.find(@event_id)
-    @competitors = @event.competitors
+    @competitors = @event.competitors.sort_by { |c| c.id }
   end
 
 end
