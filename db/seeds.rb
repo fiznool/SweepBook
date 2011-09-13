63ccea22e3168713d7a83b0422aa015dc958e5e3
Tom Spencer <tom.spencer@iscanetworks.com>
2011-09-13 19:58:00 +0100

    Added seed values for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+timeNow = Time.now
+Event.create(:id => 1, :name => 'GNR 2011', :date => timeNow, :info => 'Joe and Spens amazing GNR 2011', :forcewallpostsoff => false, :forceanonymousdonations => false, :created_at => timeNow, :updated_at => timeNow, :time_min => 120, :time_max => 180, :time_interval => 1)
+Competitor.create(:id => 2, :event_id => 1, :facebook_id => '123456', :name => 'Joe', :donation_link => 'http://www.justgiving.com/geeksgorunning', :created_at => timeNow, :updated_at => timeNow)
+Competitor.create(:id => 3, :event_id => 1, :facebook_id => 'fiznool', :name => 'Spen', :donation_link => 'http://www.justgiving.com/geeksgorunning', :created_at => timeNow, :updated_at => timeNow)
